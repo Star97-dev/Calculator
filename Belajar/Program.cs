1688b57f36fc897209e4a2eb69ff4db32c2d76cd
Star97-dev <satriaanugerah94@gmail.com>
2021-09-13 17:38:52 +0700

    Add project files.

---
+++ Belajar/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Belajar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== CALCULATOR WITH ME ===\n");
+
+            double input = 0;
+            double input2 = 0;
+            string ope = "";
+            double value = 0;
+
+            Console.Write("Input first number = ");
+            input = double.Parse(Console.ReadLine());
+            Console.Write("Input operator = ");
+            ope = Console.ReadLine();
+            Console.Write("Input second number = ");
+            input2 = double.Parse(Console.ReadLine());
+            
+
+            if (ope == "+")
+            {
+                value = input + input2;
+            } 
+            else if (ope == "-")
+            {
+                value = input - input2;
+            }
+            else if (ope == "*")
+            {
+                value = input * input2;
+            }
+            else if (ope == "/")
+            {
+                value = input / input2;
+            }
+            else if (ope != "+")
+            {
+                Console.WriteLine("Operator tidak sesuai!");
+
+            }
+            Console.Write("Value = ");
+            Console.Write(value);
+        }
+    }
+}
